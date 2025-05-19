70cd1ed4f0a66afd451eed7c8ff1c43b57ad1e96
Joaco <joaco.munoz04@gmail.com>
2025-05-19 19:46:22 -0300

    fix redirect login

--- turnero/settings.py
+++ turnero/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_URL = '/signin/'
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
